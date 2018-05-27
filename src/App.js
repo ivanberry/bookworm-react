e937461b917d69b3fc698e015175b6246c97d7e6
ivanberry <tabvim.dev@gmail.com>
2018-05-27 16:51:40 +0800

    Add component pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router';
 
-const App = () => <div>Hello world</div>
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+
+const App = () => <div className='ui container'>
+  <Route path='/' exact component={HomePage} />
+  <Route path='/login' exact component={LoginPage} />
+</div>
 
 export default App;
