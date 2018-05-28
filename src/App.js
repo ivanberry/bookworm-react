6ed468aad2ac874cd98d46eb81402204cd825138
ivanberry <tabvim.dev@gmail.com>
2018-05-28 17:27:31 +0800

    Add authrize routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { Route } from "react-router";
 
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
+import DashboardPage from "./components/pages/DashboardPage";
+import UserRoute from "./components/routes/UserRoute";
+import GuestRoute from "./components/routes/GuestRoute";
 
 const App = () => (
   <div className="ui container">
     <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />
+    <GuestRoute path="/login" exact component={LoginPage} />
+    <UserRoute path="/dashboard" exact component={DashboardPage} />
   </div>
 );
 
