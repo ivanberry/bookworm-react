b90924b2bc856d6f481e842ea91a4b4adb715372
ivanberry <tabvim.dev@gmail.com>
2018-05-28 08:06:43 +0800

    Add global error message condition component

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,66 +1,75 @@
-import React, { Component } from 'react';
-import { Form, Button } from 'semantic-ui-react';
-import PropTypes from 'prop-types';
-import Validate from 'validator';
-import InlineError from '../messages/InlineError';
+import React, { Component } from "react";
+import { Form, Button, Message } from "semantic-ui-react";
+import PropTypes from "prop-types";
+import Validate from "validator";
+import InlineError from "../messages/InlineError";
 
 class LoginForm extends Component {
   constructor() {
     super();
     this.state = {
       data: {
-        email: '',
-        password: ''
+        email: "",
+        password: ""
       },
       loading: false,
       errors: {}
-    }
+    };
   }
 
-  onChange = e => this.setState({
-    data: { ...this.state.data, [e.target.name]: e.target.value }
-  });
+  onChange = e =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
 
   onsubmit = () => {
     const errors = this.validate(this.state.data);
-    this.setState({errors});
+    this.setState({ errors });
     if (Object.keys(errors).length === 0) {
-      this.props.submit(this.state.data);
+      this.props
+        .submit(this.state.data)
+        .catch(err => this.setState({ errors: err.response.data.errors }));
     }
   };
-  validate = (data) => {
+  validate = data => {
     const errors = {};
-    if(!Validate.isEmail(data.email)) errors.email = 'Invalid Email';
-    if (!data.password) errors.password = 'Cant be blank';
+    if (!Validate.isEmail(data.email)) errors.email = "Invalid Email";
+    if (!data.password) errors.password = "Cant be blank";
     return errors;
-  }
+  };
 
   render() {
     const { data, errors } = this.state;
     return (
       <Form onSubmit={this.onsubmit}>
+        {errors.global && (
+          <Message>
+            <Message.Header>Something going wrong</Message.Header>
+            <p>{errors.global}</p>
+          </Message>
+        )}
         <Form.Field error={!!errors.email}>
-          <label htmlFor='email'>Email</label>
+          <label htmlFor="email">Email</label>
           <input
-            type='email'
-            id='email'
-            name='email'
-            placeholder='example@example.com'
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@example.com"
             value={data.email}
             onChange={this.onChange}
           />
-          {errors.email && <InlineError text='Invalid Email' />}
+          {errors.email && <InlineError text="Invalid Email" />}
         </Form.Field>
         <Form.Field error={!!errors.password}>
-          <label htmlFor='password'>password</label>
+          <label htmlFor="password">password</label>
           <input
-            type='password'
-            id='password'
-            name='password'
+            type="password"
+            id="password"
+            name="password"
             value={data.password}
             onChange={this.onChange}
           />
-          {errors.password && <InlineError text='Password cant be blank' />}
+          {errors.password && <InlineError text="Password cant be blank" />}
         </Form.Field>
         <Button primary>Login</Button>
       </Form>
@@ -69,7 +78,7 @@ class LoginForm extends Component {
 }
 
 LoginForm.propTypes = {
-  submit: PropTypes.func.isRequired,
+  submit: PropTypes.func.isRequired
 };
 
 export default LoginForm;
